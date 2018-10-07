a26035ecf65fa1f191786b7edf3886de6ecef940
Ouessai Abdessamed <ouessai.aes@gmail.com>
2018-10-07 22:26:56 +0100

    Add second SQL query

--- queries.py
+++ queries.py
@@ -8,3 +8,12 @@ GROUP BY title
 ORDER BY NBViews desc
 LIMIT 3;
 '''
+
+QUERY2 = '''
+SELECT name, count(*) as num
+FROM authors, articles, log
+WHERE authors.id = articles.author
+    AND log.path like ('%' || articles.slug)
+GROUP BY name
+ORDER BY num desc;
+'''
