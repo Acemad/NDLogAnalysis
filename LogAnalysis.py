6cef1467e2c3201c916a10a80ebfffca70c11cc5
Ouessai Abdessamed <ouessai.aes@gmail.com>
2018-10-07 22:04:21 +0100

    Add function to query the news database, queryDB

--- LogAnalysis.py
+++ LogAnalysis.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
+import psycopg2
 
 
 def main():
     print('Hello\n')
+
+
+def queryDB(query):
+    db = psycopg2.connect('dbname=news')
+    cursor = db.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    db.close()
+    return rows
 
 
 if __name__ == '__main__':
