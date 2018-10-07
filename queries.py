d7007a545d747ae9d7bb036de45952ac4f89cad7
Ouessai Abdessamed <ouessai.aes@gmail.com>
2018-10-08 00:01:37 +0100

    Add third SQL query

--- queries.py
+++ queries.py
@@ -5,15 +5,29 @@ WHERE log.path like ('%' || articles.slug)
     AND status = '200 OK'
     AND path != '/'
 GROUP BY title
-ORDER BY NBViews desc
+ORDER BY NBViews DESC
 LIMIT 3;
 '''
 
 QUERY2 = '''
-SELECT name, count(*) as num
+SELECT name, count(*) as NBViews
 FROM authors, articles, log
 WHERE authors.id = articles.author
     AND log.path like ('%' || articles.slug)
 GROUP BY name
-ORDER BY num desc;
+ORDER BY NBViews DESC;
+'''
+
+QUERY3 = '''
+SELECT to_char(date,'Month DD, YYYY'), trunc(cast(rate AS numeric),2)
+FROM
+ (
+  SELECT req_day.date,
+    (
+      (cast(err_day.reqs AS real) * 100) / cast(req_day.reqs AS real)
+    ) AS rate
+  FROM req_day, err_day
+  WHERE req_day.date = err_day.date
+ ) AS subq
+WHERE rate > 1.0;
 '''
